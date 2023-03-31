1df18ab05ff8afc2688b9ea67977cab076619024
김광민 <kmkim4238@gmail.com>
2023-03-31 23:22:12 +0900

    230331 nextjs config 변경

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const settings = {
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640],
-    domains: ["firebasestorage.googleapis.com", "maps.googleapis.com"],
+    domains: ["firebasestorage.googleapis.com", "maps.googleapis.com", "*"],
   },
 };
 
